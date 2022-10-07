c4b9fe3b5230e1a5fd1a7ab137f8ba013ebfada6
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-06 21:47:30 -0400

    validating email and password

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from "express";
 import connectDatabase from "./src/database/db.js";
-import userRoute from "./src/routes/user.route.js"; // importando todas as rotas
 import dotenv from "dotenv";
+
+//ROTAS
+import userRoute from "./src/routes/user.route.js"; // importando todas as rotas
+import authRoute from "./src/routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 connectDatabase();
 app.use(express.json()); // aplicação apta para enviar e receber arquivos json
 app.use("/user", userRoute); // usando a rota
+app.use("/auth", authRoute);
 
 app.listen(port, () => console.log(`Rodando na porta ${port}`));
