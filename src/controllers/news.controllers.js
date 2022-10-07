5c36217fff0e7539df2c8d8d80d988374a240ed9
Renata Karonlina <renatakarolinarko@gmail.com>
2022-10-07 17:44:41 -0400

    create authorizarion Bearer

--- src/controllers/news.controllers.js
+++ src/controllers/news.controllers.js
@@ -2,8 +2,26 @@ import { createService, findAllService } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
-    const { title, text, banner } = req.body;
+    const { authorization } = req.headers;
+    console.log(authorization);
 
+    if (!authorization) {
+      res.send(401);
+    }
+
+    const parts = authorization.split(" ");
+
+    if (parts.length !== 2) {
+      return res.send(401);
+    }
+
+    const [schema, token] = parts;
+
+    if (schema !== "Bearer") {
+      return res.send(401);
+    }
+
+    const { title, text, banner } = req.body;
     if (!title || !text || !banner) {
       res.status(400).send({ message: "Submit all fields for registration" });
     }
@@ -12,19 +30,28 @@ const create = async (req, res) => {
       title,
       text,
       banner,
-      id: "object id fake",
+      user: { _id: "633f835dbb6c4ef7dfed81d6" },
     });
 
-    res.send(201);
+    res.status(200).send({
+      message: "News created",
+      title,
+      text,
+      banner,
+    });
   } catch {
     res.status(500).send({ message: error.message });
   }
 };
 
-const findAll = (req, res) => {
-  const news = [];
+const findAll = async (req, res) => {
+  const news = await findAllService();
+
+  if (news.length === 0) {
+    res.send({ message: "There are no registered news" });
+  }
 
   res.send({ news });
 };
 
-export default { create, findAll };
+export { create, findAll };
