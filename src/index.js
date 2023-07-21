4356e521d5e69a64537bc6ac1d766f400a254256
Renata Karonlina <renatakarolinarko@gmail.com>
2023-07-21 11:50:20 -0400

    updated cors

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "https://api-breaknews-8891.onrender.com",
+    origin: ["https://breaknews-app.vercel.app/", "http://localhost:5173/"],
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
   })
 );
